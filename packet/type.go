13abf59aa9cc2ca21e43e018221d540a3f53dabc
Keng <keng@undesoft.com>
2018-07-11 16:33:51 +0800

    添加通讯协议

--- packet/type.go
+++ packet/type.go
@@ -3,8 +3,8 @@ package packet
 const (
 	// TypeQrs 心跳包
 	TypeQrs byte = 1
-	// TypeData 数据包
-	TypeData byte = 2
 	// TypeClose 关闭包
-	TypeClose byte = 3
+	TypeClose byte = 2
+	// TypeData 数据包
+	TypeData byte = 3
 )
